6e4f936e4c165fd3255d71739221fc5396c54018
t0k0h0r <ttt.g.yyyyyyyyy@gmail.com>
2021-05-18 23:19:50 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import { View, Text, StyleSheet } from 'react-native';
 
-import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailsScreen from './src/screens/MemoDetailsScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoDetailsScreen from './src/screens/MemoDetailsScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoDetailsScreen />
+    <MemoCreateScreen />
   );
 }
